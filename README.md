7b2c38082c171268be2bba4a2cfc9a653f84edd2
ddfilipov <denisfilipov12+3@gmail.com>
2021-11-29 22:17:36 +0100

    added styled components and a breakpoints file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Star Wars API test app
 
 React project created in order to practice navigation between components.
+
+## Dependencies using
+
+### Styled Components
+
+### React Router Dom
+
+### Axios
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { device } from "./utils/breakpoints";
 
+const Container = styled.div`
+    display: grid;
+    /* background-color: red; */
+    /* border: 1px solid black; */
+    row-gap: 10px;
 
+    div {
+        border: 1px solid black;
+        /* background-color: chartreuse; */
+        align-self: center;
+        text-align: center;
+        padding: 20px 10px 20px 10px;
+        margin: 0 10px 0 10px;
+        box-shadow: 0 1px 2px 0 rgba(173, 173, 173, 0.2), 0 1.5px 5px 0 rgba(173, 173, 173, 0.2);
+        a {
+            display: grid;
+            text-decoration: inherit;
+        }
+    }
+
+    @media only screen and (${device.md}) {
+    }
+`;
 
 function App() {
     return (
-        <div className="App">
+        <Container className="App">
             <div>
                 <Link to="/people">People</Link>
             </div>
@@ -16,7 +40,7 @@ function App() {
             <div>
                 <Link to="/planets">Planets</Link>
             </div>
-        </div>
+        </Container>
     );
 }
 
