d71b1fdd33bbd38620026134fe8840b446c65e13
ddfilipov <denisfilipov12+3@gmail.com>
2021-11-29 23:19:30 +0100

    doing a map doesn't seem to work

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,17 @@ import { device } from "./utils/breakpoints";
 
 const Container = styled.div`
     display: grid;
+    grid-template-columns: 300px;
+    justify-content: center;
     /* background-color: red; */
     /* border: 1px solid black; */
     row-gap: 10px;
+    max-width: 1440px;
+    margin: 8rem auto 4rem auto;
+
+    h1 {
+        text-align: center;
+    }
 
     div {
         border: 1px solid black;
@@ -31,6 +39,7 @@ const Container = styled.div`
 function App() {
     return (
         <Container className="App">
+            <h1>Star Wars Database</h1>
             <div>
                 <Link to="/people">People</Link>
             </div>
--- src/components/People.tsx
+++ src/components/People.tsx
@@ -11,20 +11,26 @@ const baseURL = "https://swapi.dev/api/";
 
 export default function People() {
     const [person, setPerson] = useState<PersonType>();
+    // const [people, setPeople] = useState<PersonType[]>([]);
 
     useEffect(() => {
         axios.get(`${baseURL}/people/1`).then((response) => {
             setPerson(response.data);
+            console.log(response.data);
         });
-    }, [person]);
+    }, []);
 
     if (!person) return null;
 
     return (
         <div>
-            <span>Name: {person.name}</span>
-            <span>Name: {person.height}</span>
-            <span>Name: {person.gender}</span>
+            {/* {people.map((persona) => ( */}
+            <ul>
+                <li>Name: {person.name}</li>
+                <li>Height: {person.height}</li>
+                <li>Gender: {person.gender}</li>
+            </ul>
+            {/* ))} */}
         </div>
     );
 }
